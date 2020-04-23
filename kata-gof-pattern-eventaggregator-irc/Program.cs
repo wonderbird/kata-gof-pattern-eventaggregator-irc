a3461c03372a3f4eb42ab662b325dce064e0a30f
Stefan Boos <kontakt@boos.systems>
2020-04-23 06:08:32 +0200

    Add project files.

---
+++ kata-gof-pattern-eventaggregator-irc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kata_gof_pattern_eventaggregator_irc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
